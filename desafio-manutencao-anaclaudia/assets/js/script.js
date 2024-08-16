0749998d833dbedd19eac60487d22e4ebe1674b4
Ana Claudia <anaclaudiasantanadev@gmail.com>
2024-08-16 13:55:01 -0300

    First commit

---
+++ desafio-manutencao-anaclaudia/assets/js/script.js
@@ -0,0 +1,9 @@
+function click(){
+    alert('Será um prazer atendê-lo(a).')
+    const email = prompt("Digite o seu e-mail")
+}
+
+const buttonBanner = document.getElementById('buttom_banner')
+
+buttonBanner.addEventListener('click', click)
+ 
